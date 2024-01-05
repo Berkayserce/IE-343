82ec27f737bbe07cdbaa2b880f1c697c9ba3f56a
Berkayserce <154592150+Berkayserce@users.noreply.github.com>
2024-01-05 15:36:02 +0300

    Update csv_parser.py

--- pythonfiles/csv_parser.py
+++ pythonfiles/csv_parser.py
@@ -1,32 +1,36 @@
-import City
-import Song
+from City import City
+from Song import Song
+import csv
 class CSVParser:
     @staticmethod
     def parse_cities(file_name):
         cities = []
         with open(file_name, 'r') as file:
-            header_skipped = False
-            for line in file:
-                if not header_skipped:
-                    header_skipped = True
-                    continue
-                data = line.strip().split(',')
-                city_name = data[0].strip()
-                latitude = float(data[2].strip())
-                longitude = float(data[3].strip())
-                city = City(city_name, latitude, longitude)
+            reader = csv.reader(file)
+            next(reader)  # Skip header
+            for row in reader:
+                city = City(row[0].strip(), float(row[2].strip()), float(row[3].strip()), float(row[4].strip()))
                 cities.append(city)
         return cities
 
     @staticmethod
-    def parse_songs(csv_file):
+    
+    def parse_songs(file_name):
         songs = []
-        with open(csv_file, 'r') as file:
-            next(file)  # Skip header
-            for line in file:
-                data = line.strip().split(',')
-                popularity = float(data[1])
-                duration = float(data[2])
-                song = Song(popularity, duration)
-                songs.append(song)
+        with open(file_name, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            for row in reader:
+                try:
+                    if len(row) == 3:
+                        song_name = row[0].strip()
+                        popularity = float(row[1].strip())
+                        duration = float(row[2].strip())
+                        songs.append(Song(song_name, popularity, duration))
+                    else:
+                        pass
+                   
+                except ValueError as e:
+                    print(f"Error processing row {row}: {e}")
         return songs
+
