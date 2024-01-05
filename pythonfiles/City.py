b6e43ca92344600cdafe81b6120363604425f783
Berkayserce <154592150+Berkayserce@users.noreply.github.com>
2024-01-05 15:31:21 +0300

    Update City.py

--- pythonfiles/City.py
+++ pythonfiles/City.py
@@ -1,12 +1,13 @@
 class City:
-    def __init__(self, name, latitude, longitude):
+    def __init__(self, name, latitude, longitude, concert_duration):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
+        self.concert_duration = concert_duration
 
     def distance_to(self, other_city):
-        x_distance = abs(self.latitude - other_city.get_x())
-        y_distance = abs(self.longitude - other_city.get_y())
+        x_distance = abs(self.latitude - other_city.latitude)
+        y_distance = abs(self.longitude - other_city.longitude)
         return ((x_distance ** 2) + (y_distance ** 2)) ** 0.5
 
     def get_name(self):
