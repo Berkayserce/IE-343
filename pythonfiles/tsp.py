9878f6adedb55c32c1c30857090b68e352753545
Berkayserce <154592150+Berkayserce@users.noreply.github.com>
2024-01-05 15:07:47 +0300

    Update tsp.py

--- pythonfiles/tsp.py
+++ pythonfiles/tsp.py
@@ -3,12 +3,24 @@ class TSP:
     def nearest_neighbor_tsp(cities):
         num_cities = len(cities)
         visited = [False] * num_cities
-        tour = []
-
-        current_city = cities[0]  # Start from the first city
+        tour = [cities[0]]
         visited[0] = True
-        # Your algorithm here
 
-        # TSP algorithm implementation
-        # ... (use the previously provided nearest_neighbor_tsp function here)
-        return tour  # Return the tour of cities
+        current_city = cities[0]
+
+        while len(tour) < num_cities:
+            nearest_city = None
+            min_distance = float('inf')
+
+            for i in range(num_cities):
+                if not visited[i]:
+                    distance = current_city.distance_to(cities[i])
+                    if distance < min_distance:
+                        nearest_city = cities[i]
+                        min_distance = distance
+
+            visited[cities.index(nearest_city)] = True
+            tour.append(nearest_city)
+            current_city = nearest_city
+
+        return tour
